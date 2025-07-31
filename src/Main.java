b0548ad658e8f240b5d6766b0ff27fe6afae2ed7
muhammadhadi2k5 <muhammadhadi.2k5@gmail.com>
2025-07-31 09:35:28 +0500

    init

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.io
+public class Main {
+    public static void main(String[] args) {
+
+        String currentDir = System.getProperty("user.dir");
+        System.out.println(currentDir);
+
+    }
+}
