b0a0b90a497b7376e03bac0e75c69b805ec2828f
muhammadhadi2k5 <muhammadhadi.2k5@gmail.com>
2025-07-31 11:53:37 +0500

    feat: check and extract extensions and create folders based on those extensions

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,39 @@ public class Main {
         for (File file : files){
             //only checking files not folders
             if (file.isFile()) {
-                System.out.println(file.getName());
+                String fileName = file.getName();
+
+                //get index of last dot to get the extension
+                int dotIndex = fileName.lastIndexOf('.');
+
+                /*ignore files with a starting dot and ending dot
+                only want files that have extensions like .jpg .jpeg
+                 */
+                if (dotIndex > 0 && dotIndex < files.length-1){
+                    //add the extension to a string and remove dot
+                    String extension = fileName.substring(dotIndex +1).toLowerCase();
+
+                    //categorize the files based on their extension
+                    String category = switch (extension){
+                        case "jpg","jpeg","png","gif","bmp"   -> "Images";
+                        case "mp4","avi","mkv","mov","flv"    -> "Videos";
+                        case "mp3","wav","ogg","flac"         -> "Audio";
+                        case "pdf","docx","txt","pptx","xlsx" -> "Documents";
+                        default                               -> "Others";
+                    };
+
+                    //make folder category wise if folder doesn't exist already
+                    File categoryFolder = new File(category);
+                    if (!categoryFolder.exists()){
+                        boolean created = categoryFolder.mkdir();
+                        if (created){
+                            System.out.println("Created folder: " + category);
+                        }
+                        else {
+                            System.out.println("Failed to create folder: " + category);
+                        }
+                    }
+                }
             }
         }
     }
