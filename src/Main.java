c21de6831ec87a94609b7851d58610b9dbd44deb
muhammadhadi2k5 <muhammadhadi.2k5@gmail.com>
2025-07-31 10:34:20 +0500

    feat: added comments and safety check for empty directories

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,24 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
 
+        // getting the current directory
         File currentDir = new File(".");
+
+        //make an array and get all the files inside it
         File[] files = currentDir.listFiles();
+
+        //safety check in case listFiles() returns null
+        if (files == null){
+            System.out.println("Directory is empty or inaccessible.");
+            return;
+        }
+
+        //iterate over each item
         for (File file : files){
+            //only checking files not folders
             if (file.isFile()) {
                 System.out.println(file.getName());
             }
         }
-
-
     }
 }
