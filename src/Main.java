23bde272550961ee6f2e1e47f7c564a11cf8d362
muhammadhadi2k5 <muhammadhadi.2k5@gmail.com>
2025-07-31 09:59:52 +0500

    feat: lists all the files in the current directory

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,14 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
 
-        String currentDir = System.getProperty("user.dir");
-        System.out.println(currentDir);
+        File currentDir = new File(".");
+        File[] files = currentDir.listFiles();
+        for (File file : files){
+            if (file.isFile()) {
+                System.out.println(file.getName());
+            }
+        }
+
 
     }
 }
